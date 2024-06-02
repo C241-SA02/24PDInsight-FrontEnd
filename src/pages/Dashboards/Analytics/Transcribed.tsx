208044b033693d58d3a0d7ec8039cb84cde1eac5
alkaalvinn <alkaalvinn@student.ub.ac.id>
2024-06-02 22:25:21 +0700

    Transcribed

--- src/pages/Dashboards/Analytics/Transcribed.tsx
+++ src/pages/Dashboards/Analytics/Transcribed.tsx
@@ -11,31 +11,11 @@ const Transcribed = () => {
                 <div className="card-body">
                     <div className="flex items-center gap-2">
                         <h6 className="mb-3 text-15 grow">Transcribed</h6>
-                        {/* <Dropdown className="relative dropdown shrink-0">
-                            <Dropdown.Trigger type="button" className="px-2 py-1.5 text-xs bg-text-slate-500 btn bg-slate-100 hover:text-white hover:bg-slate-600 focus:text-white focus:bg-slate-600 focus:ring focus:ring-slate-100 active:text-white active:bg-slate-600 active:ring active:ring-slate-100 dark:bg-slate-500/20 dark:text-slate-400 dark:hover:bg-slate-500 dark:hover:text-white dark:focus:bg-slate-500 dark:focus:text-white dark:active:bg-slate-500 dark:active:text-white dark:ring-slate-400/20 dropdown-toggle" id="emailDataDropdown" data-bs-toggle="dropdown">
-                                This Yearly <ChevronDown className="inline-block size-4 ltr:ml-1 rlt:mr-1"></ChevronDown>
-                            </Dropdown.Trigger>
-
-                            <Dropdown.Content placement="right-end" className="absolute z-50 py-2 mt-1 ltr:text-left rtl:text-right list-none bg-white rounded-md shadow-md dropdown-menu min-w-[10rem] dark:bg-zink-600" aria-labelledby="emailDataDropdown">
-                                <li>
-                                    <Link className="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" to="#!">1 Weekly</Link>
-                                </li>
-                                <li>
-                                    <Link className="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" to="#!">1 Monthly</Link>
-                                </li>
-                                <li>
-                                    <Link className="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" to="#!">3 Monthly</Link>
-                                </li>
-                                <li>
-                                    <Link className="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" to="#!">6 Monthly</Link>
-                                </li>
-                                <li>
-                                    <Link className="block px-4 py-1.5 text-base transition-all duration-200 ease-linear text-slate-600 dropdown-item hover:bg-slate-100 hover:text-slate-500 focus:bg-slate-100 focus:text-slate-500 dark:text-zink-100 dark:hover:bg-zink-500 dark:hover:text-zink-200 dark:focus:bg-zink-500 dark:focus:text-zink-200" to="#!">This Yearly</Link>
-                                </li>
-                            </Dropdown.Content>
-                        </Dropdown> */}
                     </div>
-                    {/* <InteractionChart chartId="transcribed" data-chart-colors='["bg-custom-500", "bg-purple-500"]' /> */}
+                    <div>
+                        <p>kita harus bersyukur di tahun 2020 [Angka] sampai 2030 [Angka] nanti kita akan mendapatkan bonus demografi . saat itulah sebagian besar penduduk kita ada pada usia produktif . ini kesempatan kita untuk meningkatkan produktivitas nasional . peluang untuk menuju indonesia [Entitas Geologi] emas makin terbuka lebar . tapi bapak - ibu yang saya hormat ##i , teman - teman sesama anak muda , ingat , kesempatan ini hanya datang sekali . kesempatan ini tidak akan ter ##ulang lagi . untuk itu kita harus kerja keras , kerja fokus , berani melakukan lompatan . saya uc ##apkan terima kasih kepada pak [Orang] prabowo [Orang] , yang sudah memberi saya kesempatan untuk ikut andil dalam kontes ##tasi ini . saya sangat bangga sekali saya menjadi bagian dalam perjalanan menuju indonesia [Entitas Geologi] emas [Entitas Geologi] . saya uc ##apkan terima kasih juga prof [Orang] . mah [Orang] ##fu ##d , gu ##z ##mu ##hay ##min [Orang] . saya sangat senang sekali bisa satu [Angka] panggung dengan orang - orang hebat seperti ini . senang sekali anak muda bisa bertukar pikiran dengan ketua umum partai [Organisasi] dan seorang profesor . sekali lagi terima kasih . anak - anak muda harus saling mendukung , anak - anak muda harus saling berga ##nd ##engan tangan . saya yakin indo [Orang] ins [Orang] ##yar ##mus bisa tercapai . terima kasih . wass ##alam ##u alai [Orang] ##kum wara ##h ##mat ##ullah ##i wa ##bara ##kat ##uh . selamat natal [Orang] dan tahun baru . terima kasih telah menonton !</p>
+                    </div>
+                    
                 </div>
             </div>
         </React.Fragment>

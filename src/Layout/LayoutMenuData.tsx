49de4e355005008ab0b064d93f216dc58dd07c45
hanifsetyadi <hfsetyadi21@gmail.com>
2024-06-06 14:22:45 +0700

    fixing upload page

--- src/Layout/LayoutMenuData.tsx
+++ src/Layout/LayoutMenuData.tsx
@@ -73,12 +73,12 @@ const menuData: any = [
         icon: <History />,
         link: '/history',
     },
-    {
-        id: 'email',
-        label: 'Dummy',
-        // icon: <Map />,
-        link: '/dashboards-email'
-    },
+    // {
+    //     id: 'email',
+    //     label: 'Dummy',
+    //     // icon: <Map />,
+    //     link: '/dashboards-email'
+    // },
     // {
     //     label: 'Apps',
     //     isTitle: true,
@@ -400,225 +400,225 @@ const menuData: any = [
     //     label: 'Pages',
     //     isTitle: true,
     // },
-    {
-        id: 'authentication',
-        label: 'Authentication',
-        // icon: <Award />,
-        parentId: 2,
-        subItems: [
-            {
-                id: 'login',
-                label: 'Login',
-                parentId: 'social',
-                subItems: [
-                    {
-                        id: 'basic',
-                        label: 'Basic',
-                        link: '/auth-login-basic',
-                        parentId: 'login'
-                    },
-                    {
-                        id: 'cover',
-                        label: 'Cover',
-                        link: '/auth-login-cover',
-                        parentId: 'login'
-                    },
-                    {
-                        id: 'boxed',
-                        label: 'Boxed',
-                        link: '/auth-login-boxed',
-                        parentId: 'login'
-                    },
-                    {
-                        id: 'modern',
-                        label: 'Modern',
-                        link: '/auth-login-modern',
-                        parentId: 'login'
-                    },
-                ]
-            },
-            {
-                id: 'register',
-                label: 'Register',
-                parentId: 'social',
-                subItems: [
-                    {
-                        id: 'registerbasic',
-                        label: 'Basic',
-                        link: '/auth-register-basic',
-                        parentId: 'register'
-                    },
-                    {
-                        id: 'registercover',
-                        label: 'Cover',
-                        link: '/auth-register-cover',
-                        parentId: 'register'
-                    },
-                    {
-                        id: 'registerboxed',
-                        label: 'Boxed',
-                        link: '/auth-register-boxed',
-                        parentId: 'register'
-                    },
-                    {
-                        id: 'registermodern',
-                        label: 'Modern',
-                        link: '/auth-register-modern',
-                        parentId: 'register'
-                    },
-                ]
-            },
-            {
-                id: 'verifyemail',
-                label: 'Verify Email',
-                parentId: 'social',
-                subItems: [
-                    {
-                        id: 'verifyemailbasic',
-                        label: 'Basic',
-                        link: '/auth-verify-email-basic',
-                        parentId: 'verifyemail'
-                    },
-                    {
-                        id: 'verifyemailcover',
-                        label: 'Cover',
-                        link: '/auth-verify-email-cover',
-                        parentId: 'verifyemail'
-                    },
-                    {
-                        id: 'verifyemailmodern',
-                        label: 'Modern',
-                        link: '/auth-verify-email-modern',
-                        parentId: 'verifyemail'
-                    },
-                ]
-            },
-            {
-                id: 'twostep',
-                label: 'Two Steps',
-                parentId: 'social',
-                subItems: [
-                    {
-                        id: 'twostepbasic',
-                        label: 'Basic',
-                        link: '/auth-two-steps-basic',
-                        parentId: 'twostep'
-                    },
-                    {
-                        id: 'twostepcover',
-                        label: 'Cover',
-                        link: '/auth-two-steps-cover',
-                        parentId: 'twostep'
-                    },
-                    {
-                        id: 'twostepboxed',
-                        label: 'Boxed',
-                        link: '/auth-two-steps-boxed',
-                        parentId: 'twostep'
-                    },
-                    {
-                        id: 'twostepmodern',
-                        label: 'Modern',
-                        link: '/auth-two-steps-modern',
-                        parentId: 'twostep'
-                    },
-                ]
-            },
-            {
-                id: 'logout',
-                label: 'Logout',
-                parentId: 'social',
-                subItems: [
-                    {
-                        id: 'logoutbasic',
-                        label: 'Basic',
-                        link: '/auth-logout-basic',
-                        parentId: 'logout'
-                    },
-                    {
-                        id: 'logoutcover',
-                        label: 'Cover',
-                        link: '/auth-logout-cover',
-                        parentId: 'logout'
-                    },
-                    {
-                        id: 'logoutboxed',
-                        label: 'Boxed',
-                        link: '/auth-logout-boxed',
-                        parentId: 'logout'
-                    },
-                    {
-                        id: 'logoutmodern',
-                        label: 'Modern',
-                        link: '/auth-logout-modern',
-                        parentId: 'logout'
-                    },
-                ]
-            },
-            {
-                id: 'resetpw',
-                label: 'Reset Password',
-                parentId: 'social',
-                subItems: [
-                    {
-                        id: 'resetpwbasic',
-                        label: 'Basic',
-                        link: '/auth-reset-password-basic',
-                        parentId: 'resetpw'
-                    },
-                    {
-                        id: 'resetpwcover',
-                        label: 'Cover',
-                        link: '/auth-reset-password-cover',
-                        parentId: 'resetpw'
-                    },
-                    {
-                        id: 'resetpwboxed',
-                        label: 'Boxed',
-                        link: '/auth-reset-password-boxed',
-                        parentId: 'resetpw'
-                    },
-                    {
-                        id: 'resetpwmodern',
-                        label: 'Modern',
-                        link: '/auth-reset-password-modern',
-                        parentId: 'resetpw'
-                    },
-                ]
-            },
-            {
-                id: 'createpw',
-                label: 'Create Password',
-                parentId: 'social',
-                subItems: [
-                    {
-                        id: 'createpwbasic',
-                        label: 'Basic',
-                        link: '/auth-create-password-basic',
-                        parentId: 'createpw'
-                    },
-                    {
-                        id: 'createpwcover',
-                        label: 'Cover',
-                        link: '/auth-create-password-cover',
-                        parentId: 'createpw'
-                    },
-                    {
-                        id: 'createpwboxed',
-                        label: 'Boxed',
-                        link: '/auth-create-password-boxed',
-                        parentId: 'createpw'
-                    },
-                    {
-                        id: 'createpwmodern',
-                        label: 'Modern',
-                        link: '/auth-create-password-modern',
-                        parentId: 'createpw'
-                    },
-                ]
-            }
-        ]
-    },
+    // {
+    //     id: 'authentication',
+    //     label: 'Authentication',
+    //     // icon: <Award />,
+    //     parentId: 2,
+    //     subItems: [
+    //         {
+    //             id: 'login',
+    //             label: 'Login',
+    //             parentId: 'social',
+    //             subItems: [
+    //                 {
+    //                     id: 'basic',
+    //                     label: 'Basic',
+    //                     link: '/auth-login-basic',
+    //                     parentId: 'login'
+    //                 },
+    //                 {
+    //                     id: 'cover',
+    //                     label: 'Cover',
+    //                     link: '/auth-login-cover',
+    //                     parentId: 'login'
+    //                 },
+    //                 {
+    //                     id: 'boxed',
+    //                     label: 'Boxed',
+    //                     link: '/auth-login-boxed',
+    //                     parentId: 'login'
+    //                 },
+    //                 {
+    //                     id: 'modern',
+    //                     label: 'Modern',
+    //                     link: '/auth-login-modern',
+    //                     parentId: 'login'
+    //                 },
+    //             ]
+    //         },
+    //         {
+    //             id: 'register',
+    //             label: 'Register',
+    //             parentId: 'social',
+    //             subItems: [
+    //                 {
+    //                     id: 'registerbasic',
+    //                     label: 'Basic',
+    //                     link: '/auth-register-basic',
+    //                     parentId: 'register'
+    //                 },
+    //                 {
+    //                     id: 'registercover',
+    //                     label: 'Cover',
+    //                     link: '/auth-register-cover',
+    //                     parentId: 'register'
+    //                 },
+    //                 {
+    //                     id: 'registerboxed',
+    //                     label: 'Boxed',
+    //                     link: '/auth-register-boxed',
+    //                     parentId: 'register'
+    //                 },
+    //                 {
+    //                     id: 'registermodern',
+    //                     label: 'Modern',
+    //                     link: '/auth-register-modern',
+    //                     parentId: 'register'
+    //                 },
+    //             ]
+    //         },
+    //         {
+    //             id: 'verifyemail',
+    //             label: 'Verify Email',
+    //             parentId: 'social',
+    //             subItems: [
+    //                 {
+    //                     id: 'verifyemailbasic',
+    //                     label: 'Basic',
+    //                     link: '/auth-verify-email-basic',
+    //                     parentId: 'verifyemail'
+    //                 },
+    //                 {
+    //                     id: 'verifyemailcover',
+    //                     label: 'Cover',
+    //                     link: '/auth-verify-email-cover',
+    //                     parentId: 'verifyemail'
+    //                 },
+    //                 {
+    //                     id: 'verifyemailmodern',
+    //                     label: 'Modern',
+    //                     link: '/auth-verify-email-modern',
+    //                     parentId: 'verifyemail'
+    //                 },
+    //             ]
+    //         },
+    //         {
+    //             id: 'twostep',
+    //             label: 'Two Steps',
+    //             parentId: 'social',
+    //             subItems: [
+    //                 {
+    //                     id: 'twostepbasic',
+    //                     label: 'Basic',
+    //                     link: '/auth-two-steps-basic',
+    //                     parentId: 'twostep'
+    //                 },
+    //                 {
+    //                     id: 'twostepcover',
+    //                     label: 'Cover',
+    //                     link: '/auth-two-steps-cover',
+    //                     parentId: 'twostep'
+    //                 },
+    //                 {
+    //                     id: 'twostepboxed',
+    //                     label: 'Boxed',
+    //                     link: '/auth-two-steps-boxed',
+    //                     parentId: 'twostep'
+    //                 },
+    //                 {
+    //                     id: 'twostepmodern',
+    //                     label: 'Modern',
+    //                     link: '/auth-two-steps-modern',
+    //                     parentId: 'twostep'
+    //                 },
+    //             ]
+    //         },
+    //         {
+    //             id: 'logout',
+    //             label: 'Logout',
+    //             parentId: 'social',
+    //             subItems: [
+    //                 {
+    //                     id: 'logoutbasic',
+    //                     label: 'Basic',
+    //                     link: '/auth-logout-basic',
+    //                     parentId: 'logout'
+    //                 },
+    //                 {
+    //                     id: 'logoutcover',
+    //                     label: 'Cover',
+    //                     link: '/auth-logout-cover',
+    //                     parentId: 'logout'
+    //                 },
+    //                 {
+    //                     id: 'logoutboxed',
+    //                     label: 'Boxed',
+    //                     link: '/auth-logout-boxed',
+    //                     parentId: 'logout'
+    //                 },
+    //                 {
+    //                     id: 'logoutmodern',
+    //                     label: 'Modern',
+    //                     link: '/auth-logout-modern',
+    //                     parentId: 'logout'
+    //                 },
+    //             ]
+    //         },
+    //         {
+    //             id: 'resetpw',
+    //             label: 'Reset Password',
+    //             parentId: 'social',
+    //             subItems: [
+    //                 {
+    //                     id: 'resetpwbasic',
+    //                     label: 'Basic',
+    //                     link: '/auth-reset-password-basic',
+    //                     parentId: 'resetpw'
+    //                 },
+    //                 {
+    //                     id: 'resetpwcover',
+    //                     label: 'Cover',
+    //                     link: '/auth-reset-password-cover',
+    //                     parentId: 'resetpw'
+    //                 },
+    //                 {
+    //                     id: 'resetpwboxed',
+    //                     label: 'Boxed',
+    //                     link: '/auth-reset-password-boxed',
+    //                     parentId: 'resetpw'
+    //                 },
+    //                 {
+    //                     id: 'resetpwmodern',
+    //                     label: 'Modern',
+    //                     link: '/auth-reset-password-modern',
+    //                     parentId: 'resetpw'
+    //                 },
+    //             ]
+    //         },
+    //         {
+    //             id: 'createpw',
+    //             label: 'Create Password',
+    //             parentId: 'social',
+    //             subItems: [
+    //                 {
+    //                     id: 'createpwbasic',
+    //                     label: 'Basic',
+    //                     link: '/auth-create-password-basic',
+    //                     parentId: 'createpw'
+    //                 },
+    //                 {
+    //                     id: 'createpwcover',
+    //                     label: 'Cover',
+    //                     link: '/auth-create-password-cover',
+    //                     parentId: 'createpw'
+    //                 },
+    //                 {
+    //                     id: 'createpwboxed',
+    //                     label: 'Boxed',
+    //                     link: '/auth-create-password-boxed',
+    //                     parentId: 'createpw'
+    //                 },
+    //                 {
+    //                     id: 'createpwmodern',
+    //                     label: 'Modern',
+    //                     link: '/auth-create-password-modern',
+    //                     parentId: 'createpw'
+    //                 },
+    //             ]
+    //         }
+    //     ]
+    // },
     // {
     //     id: 'pages',
     //     label: 'Pages',
--- src/pages/Dashboards/Analytics/index.tsx
+++ src/pages/Dashboards/Analytics/index.tsx
@@ -17,8 +17,12 @@ import SentimentAnalysis from './SentimentAnalysis';
 import NamedEntityRecognition from './NamedEntityRecognition';
 import TopicModelling from './TopicModelling';
 import Summarization from './Summarization';
+import WordCloud from './WordCloud';
 
 const Analytics = () => {
+
+  // Data for Named Entity Recognition
+  const nerAnalysis = `kita harus bersyukur di tahun 2020 [Angka] sampai 2030 [Angka] nanti kita akan mendapatkan bonus demografi . saat itulah sebagian besar penduduk kita ada pada usia produktif . ini kesempatan kita untuk meningkatkan produktivitas nasional . peluang untuk menuju indonesia [Entitas Geologi] emas makin terbuka lebar . tapi bapak - ibu yang saya hormat ##i , teman - teman sesama anak muda , ingat , kesempatan ini hanya datang sekali . kesempatan ini tidak akan ter ##ulang lagi . untuk itu kita harus kerja keras , kerja fokus , berani melakukan lompatan . saya uc ##apkan terima kasih kepada pak [Orang] prabowo [Orang] , yang sudah memberi saya kesempatan untuk ikut andil dalam kontes ##tasi ini . saya sangat bangga sekali saya menjadi bagian dalam perjalanan menuju indonesia [Entitas Geologi] emas [Entitas Geologi] . saya uc ##apkan terima kasih juga prof [Orang] . mah [Orang] ##fu ##d , gu ##z ##mu ##hay ##min [Orang] . saya sangat senang sekali bisa satu [Angka] panggung dengan orang - orang hebat seperti ini . senang sekali anak muda bisa bertukar pikiran dengan ketua umum partai [Organisasi] dan seorang profesor . sekali lagi terima kasih . anak - anak muda harus saling mendukung , anak - anak muda harus saling berga ##nd ##engan tangan . saya yakin indo [Orang] ins [Orang] ##yar ##mus bisa tercapai . terima kasih . wass ##alam ##u alai [Orang] ##kum wara ##h ##mat ##ullah ##i wa ##bara ##kat ##uh . selamat natal [Orang] dan tahun baru . terima kasih telah menonton !`;
 
   return (
     <React.Fragment>
@@ -27,8 +31,9 @@ const Analytics = () => {
         <Transcribed></Transcribed>
         <SentimentAnalysis></SentimentAnalysis>
         <TopicModelling></TopicModelling>
-        <NamedEntityRecognition></NamedEntityRecognition>
+        <NamedEntityRecognition nerAnalysis={nerAnalysis}></NamedEntityRecognition>
         <Summarization></Summarization>
+        <WordCloud></WordCloud>
         {/* <Widgets /> */}
         {/* <LocationBased /> */}
         {/* <Interaction />

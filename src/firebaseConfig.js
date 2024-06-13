40ecea01f943a9628a1a99c643cb21aa64ff777c
hanifsetyadi <hfsetyadi21@gmail.com>
2024-06-13 21:35:07 +0700

    add yaml

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA-08zqD7FKrPxxrLIZoT92kVEnOPffXbs",
-  authDomain: "pdinsight-eb5dc.firebaseapp.com",
-  projectId: "pdinsight-eb5dc",
-  storageBucket: "pdinsight-eb5dc.appspot.com",
-  messagingSenderId: "96057552774",
-  appId: "1:96057552774:web:92e1eff926d9d7d8e80607",
-  measurementId: "G-QSGD20ZMLN"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase

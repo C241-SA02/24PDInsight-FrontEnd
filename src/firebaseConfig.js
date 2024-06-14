348cd97519c9e448782307e9087a223d7505449e
hanifsetyadi <hfsetyadi21@gmail.com>
2024-06-14 14:11:36 +0700

    fixing firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECTID,
+//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_APPID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENT
+  apiKey: "AIzaSyA-08zqD7FKrPxxrLIZoT92kVEnOPffXbs",
+  authDomain: "pdinsight-eb5dc.firebaseapp.com",
+  projectId: "pdinsight-eb5dc",
+  storageBucket: "pdinsight-eb5dc.appspot.com",
+  messagingSenderId: "96057552774",
+  appId: "1:96057552774:web:92e1eff926d9d7d8e80607",
+  measurementId: "G-QSGD20ZMLN"
 };
 
 // Initialize Firebase
